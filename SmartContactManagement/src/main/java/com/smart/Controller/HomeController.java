a3596d00dbb4e8e55e675e9e59dc45a45964fb1b
amitesh <athakur9691@gmail.com>
2024-06-03 18:36:05 +0530

    Home , About and Registration through Signup is done

--- SmartContactManagement/src/main/java/com/smart/Controller/HomeController.java
+++ SmartContactManagement/src/main/java/com/smart/Controller/HomeController.java
@@ -2,9 +2,15 @@ package com.smart.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.smart.Entities.Contact;
 import com.smart.Entities.User;
 import com.smart.dao.UserRepository;
 
@@ -13,8 +19,44 @@ public class HomeController {
     @Autowired
   private UserRepository userRepository;
 
-    @GetMapping("/test")
+  @GetMapping({"home", "/"})
+
+  public String home(Model model)
+  {
+    model.addAttribute("title", "Home - Smart contact Management");
+    return "index";
+  }  
+
+  @RequestMapping("/about")
+  public String About(Model model)
+  {
+    model.addAttribute("title", "About - Smart Contact Management");
+    return "About";
+  }
+  
+
+  @RequestMapping("/signup")
+  public String Signup(Model model)
+  {
+    model.addAttribute("title", "Sign Up - Smart Contact Management");
+    model.addAttribute("user", new User());
+    return "Signup";
+  }
+  
+  //this handler for registering user
+  @PostMapping("/do_register")
+  public String registerUser(@ModelAttribute("user") User user, @RequestParam(value="aggrement", defaultValue = "false") boolean agreement, Model model)
+  {
+    System.out.println("Agreeement"+agreement);
+    System.out.println(user);
+
+    return "Signup";
+  }
+  
+  
+  @GetMapping("/test")
     @ResponseBody
+    // if we do not write @Response body then it will serach for html page of that name
     public String test()
     {
 
@@ -22,7 +64,9 @@ public class HomeController {
         user.setName("Amitesh Thakur");
         user.setEmail("athakur9691@gmail.com");
         user.setAbout("I am a student");
-       
+      
+        Contact contact= new Contact();
+        user.getContacts().add(contact);
 
         userRepository.save(user);
 
--- SmartContactManagement/src/main/java/com/smart/Entities/User.java
+++ SmartContactManagement/src/main/java/com/smart/Entities/User.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +38,9 @@ private String about;
 
 /* `@OneToMany(cascade = CascadeType.ALL)` in Spring Boot specifies a one-to-many relationship between two entities, where changes to the parent entity (like save, update, delete) are automatically applied to the related child entities. This ensures the parent and child entities stay synchronized in the database. */
 //This is for mapping with another entity.
-@OneToMany(cascade = CascadeType.ALL)
+//mappedBy= "user "  which means that it has to managed the relationship by user foreign key.
+
+@OneToMany(cascade = CascadeType.ALL , fetch = FetchType.LAZY, mappedBy="user")
 private List<Contact> contacts= new ArrayList<>();
 
 }
