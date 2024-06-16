c5920c7badd53c357e84495ec49b3a4858d9374b
Amitesh Thakur <athkaur9691@gmail.com>
2024-06-16 19:24:17 +0530

    updated files are commited

--- SmartContactManagement/src/main/java/com/smart/Controller/HomeController.java
+++ SmartContactManagement/src/main/java/com/smart/Controller/HomeController.java
@@ -1,124 +1,99 @@
 package com.smart.Controller;
 
-import com.smart.Entities.Contact;
-import com.smart.Entities.User;
-import com.smart.dao.UserRepository;
-import com.smart.helper.Message;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.Entities.User;
+import com.smart.helper.Message;
 
 @Controller
 public class HomeController {
+	
+//	@Autowired
+//    UserService userService;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
+	
+	@GetMapping(value={"/", "/home"})
+	public String home(Model model) {
+		model.addAttribute("title", " Home : smart contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About : Smart contact Manager");
+		return "about";
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title","Register : Smart contact manager");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+	
+	/* Handling Register form data */
+	@RequestMapping(value="/register/", method = RequestMethod.POST)
+	public String register(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, @RequestParam(value="agreement", defaultValue = "false" ) boolean agreement, Model model, HttpSession session) {
+		
+		try {	
+			if(bindingResult.hasErrors()) {
+				System.out.println("Error : "+bindingResult);
+				return "signup";
+			}
+			
+			if(!agreement) {
+				System.out.println("you have not agreed terms and Condition, .");
+				throw new Exception("you have not accept terms and Condition.");
+			}
+			System.out.println("agreement : "+agreement);
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			user.setImageUrl("profile.jpg");
+			user.setEnabled(true);
+			user.setRole("ROLE_USER");
 
-  @Autowired
-  private UserRepository userRepository;
+			System.out.println("Before Register User : "+user);
+			//User result = userService.userRegister(user);
+			User saveResult = userRepository.save(user);
+			System.out.println("After Registered User : "+saveResult);
+		
+			// after successfully registered,  form data must return empty  
+			User emptyUser = new User();
+			model.addAttribute("user", emptyUser);
+			session.setAttribute("message", new Message("Registration successfully", "alert-success"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("ohhh..!"+e.getMessage(), "alert-danger") );
+			return "signup";
+		}
+		return "signup";
+	}
+	
+	@GetMapping("/signin")
+	public String login(Model model) {
+		model.addAttribute("title", "Login : Smart contact Manager");
+		return "login";
+	}
+	
 
-  @GetMapping({ "home", "/" })
-  public String home(Model model) {
-    model.addAttribute("title", "Home - Smart contact Management");
-    return "index";
-  }
-
-  @RequestMapping("/about")
-  public String About(Model model) {
-    model.addAttribute("title", "About - Smart Contact Management");
-    return "About";
-  }
-
-  @RequestMapping("/signup")
-  public String Signup(Model model) {
-    model.addAttribute("title", "Sign Up - Smart Contact Management");
-    model.addAttribute("user", new User());
-    return "Signup";
-  }
-
-  //this handler for registering user
-  //@Valid triggers the validation we have applied on the Entitis.
-  // BindingResult gives the result of the vlidation we have applied.......
-  @PostMapping("/do_register")
-  public String registerUser(
-    @Valid @ModelAttribute("user") User user, BindingResult bindingResult,
-    @RequestParam(
-      value = "agreement",
-      defaultValue = "false"
-    ) boolean agreement,
-    Model model,
-    
-    HttpSession session
-  ) {
-    try {
-      if (!agreement) {
-        System.out.println("You have not agreed to terms and condition.");
-        throw new Exception("You have not agreed to terms and condition.");
-      }
-
-      //checking server side validation ......
-      if (bindingResult.hasErrors()) {
-        System.out.println("Error: "+bindingResult.toString());
-        model.addAttribute("user", user);
-        return "Signup";
-      }
-
-      user.setRole("ROLE_USER");
-      user.setEnabled(true);
-      user.setImageUrl("userImg.jpg");
-      // System.out.println("Agreeement"+agreement);
-      // System.out.println(user);
-      User result = this.userRepository.save(user);
-
-      model.addAttribute("user", new User());
-
-      session.setAttribute(
-        "message",
-        new Message("Success !!", "alert-success")
-      );
-      return "Signup";
-    } catch (Exception exception) {
-      exception.printStackTrace();
-      model.addAttribute("user", user);
-      session.setAttribute(
-        "message",
-        new Message(
-          "Something went wrong !!" + exception.getMessage(),
-          "alert-danger"
-        )
-      );
-      return "Signup";
-    }
-  }
-
-
-
-
-
-
-
-  
-
-  @GetMapping("/test")
-  @ResponseBody
-  // if we do not write @Response body then it will serach for html page of that name
-  public String test() {
-    User user = new User();
-    user.setName("Amitesh Thakur");
-    user.setEmail("athakur9691@gmail.com");
-    user.setAbout("I am a student");
-
-    Contact contact = new Contact();
-    user.getContacts().add(contact);
-
-    userRepository.save(user);
-
-    return "Working ";
-  }
 }
--- SmartContactManagement/src/main/java/com/smart/helper/Message.java
+++ SmartContactManagement/src/main/java/com/smart/helper/Message.java
@@ -1,14 +1,37 @@
 package com.smart.helper;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class Message {
+	
+	private String content;
+	private String type;	
+	
+	public Message(String content, String type) {
+		super();
+		this.content = content;
+		this.type = type;
+	}
 
-  private String content;
-  private String type;
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [content=" + content + ", type=" + type + "]";
+	}
+	
+	
+	
 }
